a7f0485892280260df9a25776c296a9b319c087b
VictorFernado <victorfernando.sis@gmail.com>
2017-09-22 04:06:14 -0300

    Opening the camera

--- app/src/main/java/com/example/acessolivre/combineclothes/CombineClothes.java
+++ app/src/main/java/com/example/acessolivre/combineclothes/CombineClothes.java
@@ -2,13 +2,28 @@ package com.example.acessolivre.combineclothes;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.widget.*;
+        import android.view.*;
+        import combineclothes.projeto.combine_clothes.R;
+        import android.content.*;
 
-public class CombineClothes extends AppCompatActivity {
+public class CombineClothes extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton makeClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(combineclothes.projeto.combine_clothes.R.layout.activity_main);
+
+        makeClick = (ImageButton)findViewById(R.id.makeClick);
+        makeClick.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v){
+        Intent it = new Intent(this, TelaCamera.class);
+        startActivity(it);
     }
 }
 
